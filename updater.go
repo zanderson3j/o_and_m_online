4da9a6b3ec66c0048eeaa59274d6b5ffa8c896da
Zachary Anderson <zacharyanderson@Zacharys-MacBook-Pro.local>
2025-12-13 20:19:07 -0500

    Bump version to 1.0.10

--- updater.go
+++ updater.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const currentVersion = "1.0.9"
+const currentVersion = "1.0.10"
 
 // checkForUpdates checks for new versions and logs the current version
 func checkForUpdates(gr *GameRoom) {
